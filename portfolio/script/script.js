4128ce23fb6bd319de5b2526a27431aa536df44b
Katsiaryna Dounar <katsiarynadounar@MacBook-Pro-Katsiaryna.local>
2026-04-02 09:11:53 +0300

    feat: add work slider on the dekstop

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -4,6 +4,8 @@ const burgerLineFirst = document.querySelector(".burger-line-first");
 const burgerLineSecond = document.querySelector(".burger-line-second");
 const navList = document.querySelectorAll(".nav-list_item");
 
+const slider = document.querySelector(".slider");
+
 function workBurgerMenu() {
   navMenu.classList.toggle("active-nav-menu");
   burgerLineFirst.classList.toggle("active-burger-line");
@@ -19,4 +21,32 @@ navList.forEach((navItem) => {
   navItem.addEventListener("click", () => {
     workBurgerMenu();
   });
+});
+
+let scrollDirection = 0;
+let scrollSpeed = 5;
+
+function autoScroll() {
+  if (scrollDirection !== 0) {
+    slider.scrollLeft += scrollDirection * scrollSpeed;
+  }
+  requestAnimationFrame(autoScroll);
+}
+
+slider.addEventListener("mousemove", (e) => {
+  const sliderWidth = slider.clientWidth;
+  const mouseX = e.clientX - slider.getBoundingClientRect().left;
+  if (mouseX < sliderWidth * 0.3) {
+    scrollDirection = 1;
+    autoScroll();
+  } else if (mouseX > sliderWidth * 0.7) {
+    scrollDirection = -1;
+    autoScroll();
+  } else {
+    scrollDirection = 0;
+  }
+});
+
+slider.addEventListener("mouseleave", () => {
+  scrollDirection = 0;
 });
