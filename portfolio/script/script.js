ea86c9058e5c17a8192a49cb2c4c7c0c737f12f8
Katsiaryna Dounar <katsiarynadounar@MacBook-Pro-Katsiaryna.local>
2026-04-07 09:31:21 +0300

    feat: add work accordion

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -6,6 +6,11 @@ const navList = document.querySelectorAll(".nav-list_item");
 
 const slider = document.querySelector(".slider");
 
+const blockFaqAnswers = document.querySelectorAll(".faq-accordion-item");
+const accordion = document.querySelectorAll(".faq-accordion-item_a");
+const plusFaq = document.querySelectorAll(".plus");
+const minusFaq = document.querySelectorAll(".minus");
+
 function workBurgerMenu() {
   navMenu.classList.toggle("active-nav-menu");
   burgerLineFirst.classList.toggle("active-burger-line");
@@ -49,4 +54,43 @@ slider.addEventListener("mousemove", (e) => {
 
 slider.addEventListener("mouseleave", () => {
   scrollDirection = 0;
+});
+
+function clickItemAccordion() {}
+
+function clickOtherItemAccordion(answer) {
+  blockFaqAnswers.forEach((otherAnswer) => {
+    if (otherAnswer !== answer) {
+      const otherTextAccordion = otherAnswer.childNodes[3];
+      const otherMinus = otherAnswer.childNodes[1].childNodes[3];
+      const otherPlus = otherAnswer.childNodes[1].childNodes[5];
+
+      otherTextAccordion.classList.remove("active-accordion");
+      otherMinus.classList.remove("active-minus");
+      otherPlus.classList.remove("active-plus");
+    }
+  });
+}
+
+blockFaqAnswers.forEach((answer) => {
+  answer.childNodes[1].addEventListener("click", () => {
+    const textAccordionCurrent = answer.childNodes[3];
+    const minusCurrent = answer.childNodes[1].childNodes[3];
+    const plusCurrent = answer.childNodes[1].childNodes[5];
+    const isActive =
+      textAccordionCurrent.classList.contains("active-accordion");
+
+    if (isActive) {
+      textAccordionCurrent.classList.remove("active-accordion");
+      minusCurrent.classList.remove("active-minus");
+      plusCurrent.classList.remove("active-plus");
+      return;
+    }
+
+    clickOtherItemAccordion(answer);
+
+    textAccordionCurrent.classList.add("active-accordion");
+    minusCurrent.classList.add("active-minus");
+    plusCurrent.classList.add("active-plus");
+  });
 });
